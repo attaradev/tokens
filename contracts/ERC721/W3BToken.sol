83bc258615e804243c27af0800049374b1e85779
Mike Attara <mpyebattara@gmail.com>
2023-09-06 09:03:14 +0000

    complete implementation

--- contracts/ERC721/W3BToken.sol
+++ contracts/ERC721/W3BToken.sol
@@ -7,8 +7,9 @@ import "./interfaces/ERC721Metadata.sol";
 import "./interfaces/ERC721TokenReceiver.sol";
 import "./interfaces/ERC721Enumerable.sol";
 
-contract W3BToken is ERC165, ERC721, ERC721Metadata, ERC721Enumerable {
+abstract contract W3BToken is ERC165, ERC721, ERC721Metadata, ERC721Enumerable {
     struct Token {
+        uint256 id;
         address owner;
         address approved;
         string uri;
@@ -18,71 +19,92 @@ contract W3BToken is ERC165, ERC721, ERC721Metadata, ERC721Enumerable {
     bytes4 private constant _INTERFACE_ID_ERC721_METADATA = 0x5b5e139f;
     bytes4 private constant _INTERFACE_ID_ERC721_RECEIVER = 0x150b7a02;
     bytes4 private constant _INTERFACE_ID_ERC721_ENUMERABLE = 0x780e9d63;
-    uint256 private _totalSupply;
-    mapping(uint256 => Token) private _tokens;
-    mapping(bytes4 => bool) private _registeredInterfaces;
-    mapping(address => uint) private _balances;
+    uint256 private tokenId;
+    address private administrator;
+    mapping(uint256 => Token) private tokens;
+    mapping(bytes4 => bool) private registeredInterfaces;
+    mapping(address => uint) private balances;
     mapping(address => mapping(address => bool)) private _operators;
 
     modifier notFalseInterfaceId(bytes4 interfaceId) {
         require(interfaceId != 0xffffffff, "ERC165: invalid interface id");
         _;
     }
-
-    modifier authorized(uint256 tokenId) {
+    modifier authorized(uint256 _tokenId) {
         require(
-            _tokens[tokenId].owner == msg.sender ||
-                _tokens[tokenId].approved == msg.sender ||
-                _operators[_tokens[tokenId].owner][msg.sender],
+            tokens[_tokenId].owner == msg.sender ||
+                tokens[_tokenId].approved == msg.sender ||
+                _operators[tokens[_tokenId].owner][msg.sender],
             "ERC721: transfer caller is not owner nor approved"
         );
         _;
     }
-
-    modifier validToken(uint256 tokenId) {
-        require(_tokens[tokenId].owner != address(0), "ERC721: invalid token");
+    modifier validToken(uint256 _tokenId) {
+        require(tokens[tokenId].owner != address(0), "ERC721: invalid token");
         _;
     }
-
     modifier notToZeroAddress(address to) {
         require(to != address(0), "ERC721: transfer to the zero address");
         _;
     }
-
-    modifier fromOwner(uint256 tokenId, address from) {
+    modifier fromOwner(uint256 _tokenId, address _from) {
         require(
-            _tokens[tokenId].owner == msg.sender,
+            tokens[_tokenId].owner == _from,
             "ERC721: transfer of token that is not owned"
         );
         _;
     }
+    modifier onlyAdmin() {
+        require(
+            msg.sender == administrator,
+            "W3B Token: caller is not the administrator"
+        );
+        _;
+    }
 
-    constructor() {
+    constructor(address _administrator) {
         // register the supported interfaces to conform to ERC721 via ERC165
         _registerInterface(_INTERFACE_ID_ERC721);
         _registerInterface(_INTERFACE_ID_ERC721_METADATA);
         _registerInterface(_INTERFACE_ID_ERC721_RECEIVER);
         _registerInterface(_INTERFACE_ID_ERC721_ENUMERABLE);
+        administrator = _administrator;
     }
 
     function _registerInterface(
         bytes4 interfaceID
     ) internal notFalseInterfaceId(interfaceID) {
-        _registeredInterfaces[interfaceID] = true;
+        registeredInterfaces[interfaceID] = true;
     }
 
     function _transfer(address _from, address _to, uint256 _tokenId) internal {
-        _tokens[_tokenId].owner = _to;
-        _balances[_from] -= 1;
-        _balances[_to] += 1;
+        tokens[_tokenId].owner = _to;
+        balances[_from] -= 1;
+        balances[_to] += 1;
 
         emit Transfer(_from, _to, _tokenId);
+    }
+
+    function _mint(address _to, string memory _uri) internal {
+        tokenId += 1;
+        tokens[tokenId] = Token({
+            id: tokenId,
+            owner: _to,
+            approved: address(0),
+            uri: _uri
+        });
+        balances[_to] += 1;
+        emit Transfer(address(0), _to, tokenId);
+    }
+
+    function mint(address _to, string memory _uri) external onlyAdmin {
+        _mint(_to, _uri);
     }
 
     function supportsInterface(
         bytes4 interfaceID
     ) external view override notFalseInterfaceId(interfaceID) returns (bool) {
-        return _registeredInterfaces[interfaceID];
+        return registeredInterfaces[interfaceID];
     }
 
     function balanceOf(
@@ -92,13 +114,13 @@ contract W3BToken is ERC165, ERC721, ERC721Metadata, ERC721Enumerable {
             _owner != address(0),
             "ERC721: balance query for the zero address"
         );
-        return _balances[_owner];
+        return balances[_owner];
     }
 
     function ownerOf(
         uint256 _tokenId
     ) external view override validToken(_tokenId) returns (address) {
-        return _tokens[_tokenId].owner;
+        return tokens[_tokenId].owner;
     }
 
     function safeTransferFrom(
@@ -115,7 +137,7 @@ contract W3BToken is ERC165, ERC721, ERC721Metadata, ERC721Enumerable {
         notToZeroAddress(_to)
         fromOwner(_tokenId, _from)
     {
-        if (_registeredInterfaces[_INTERFACE_ID_ERC721_RECEIVER]) {
+        if (registeredInterfaces[_INTERFACE_ID_ERC721_RECEIVER]) {
             bytes4 retval = ERC721TokenReceiver(_to).onERC721Received(
                 msg.sender,
                 _from,
@@ -160,11 +182,11 @@ contract W3BToken is ERC165, ERC721, ERC721Metadata, ERC721Enumerable {
         uint256 _tokenId
     ) external payable override {
         require(
-            _tokens[_tokenId].owner == msg.sender ||
-                _operators[_tokens[_tokenId].owner][msg.sender],
+            tokens[_tokenId].owner == msg.sender ||
+                _operators[tokens[_tokenId].owner][msg.sender],
             "ERC721: approve caller is not owner nor approved"
         );
-        _tokens[_tokenId].approved = _approved;
+        tokens[_tokenId].approved = _approved;
         emit Approval(msg.sender, _approved, _tokenId);
     }
 
@@ -179,7 +201,7 @@ contract W3BToken is ERC165, ERC721, ERC721Metadata, ERC721Enumerable {
     function getApproved(
         uint256 _tokenId
     ) external view override validToken(_tokenId) returns (address) {
-        return _tokens[_tokenId].approved;
+        return tokens[_tokenId].approved;
     }
 
     function isApprovedForAll(
@@ -200,21 +222,17 @@ contract W3BToken is ERC165, ERC721, ERC721Metadata, ERC721Enumerable {
     function tokenURI(
         uint256 _tokenId
     ) external view override returns (string memory) {
-        return _tokens[_tokenId].uri;
+        return tokens[_tokenId].uri;
     }
 
     function totalSupply() external view override returns (uint256) {
-        return _totalSupply;
-    }
-
-    function tokenByIndex(
-        uint256 _index
-    ) external view override validToken(_index) returns (uint256) {
-        require(
-            _index < _totalSupply,
-            "ERC721Enumerable: global index out of bounds"
-        );
-        return _index;
+        uint counter = 0;
+        for (uint i = 0; i < tokenId; i++) {
+            if (tokens[i + 1].owner != address(0)) {
+                counter += 1;
+            }
+        }
+        return counter;
     }
 
     function tokenOfOwnerByIndex(
@@ -222,9 +240,24 @@ contract W3BToken is ERC165, ERC721, ERC721Metadata, ERC721Enumerable {
         uint256 _index
     ) external view override returns (uint256) {
         require(
-            _index < _balances[_owner],
+            _index < balances[_owner],
             "ERC721Enumerable: owner index out of bounds"
         );
-        return _index;
+        return balances[_owner];
+    }
+
+    function tokenByIndex(
+        uint256 _index
+    ) external view override returns (uint256) {
+        uint currentIndex = 0;
+        for (uint i = 0; i < tokenId; i++) {
+            if (tokens[i + 1].owner != address(0)) {
+                if (currentIndex == _index) {
+                    return i + 1;
+                }
+                currentIndex += 1;
+            }
+        }
+        revert("ERC721Enumerable: global index out of bounds");
     }
 }
